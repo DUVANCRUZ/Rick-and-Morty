008b5e6830a3f59be851f7cae11de0a5f86af1d0
Duvan Cruz <duvandres9820@gmail.com>
2023-05-04 15:31:06 -0500

    back end I

--- Back/src/controllers/handleFavorites.js
+++ Back/src/controllers/handleFavorites.js
@@ -1,11 +1,19 @@
+const { User, Favorite}= require("../DB_connection")
+
+
 let myFavorites=[];
+
 
 const postFav= (req, res)=>{
     const character= req.body;
 
-    myFavorites.push(character);
+   const char = await Favorite.create(character)
+   if(userID){
+    const User = await User.findByPk(userID);
+    user ? await char.addFavorite(user): null
+   }
 
-    return res.status(200).json(myFavorites);
+    return res.status(200).json(char);
 }
 
 const deleteFav=(req, res)=>{
