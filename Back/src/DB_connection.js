21cbb9bb383bcb16b7000647730f5df4e186f010
Duvan Cruz <duvandres9820@gmail.com>
2023-05-04 12:40:17 -0500

    back end I

---
+++ Back/src/DB_connection.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const userModel = require("./models/User");
+const favModel = require("./models/Favorite")
+
+// EJERCICIO 03
+// A la instancia de Sequelize le falta la URL de conexión. ¡Agrégala!
+// Recuerda pasarle la información de tu archivo '.env'.
+
+// URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
+
+console.log(DB_USER)
+
+const sequelize = new Sequelize(
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/rickandmorty`,
+   { logging: false, native: false }
+);
+
+// EJERCICIO 05
+// Debajo de este comentario puedes ejecutar la función de los modelos.
+
+userModel(sequelize);
+favModel(sequelize)
+//
+
+
+// Ejercicio 06
+// ¡Relaciona tus modelos aquí abajo!
+const { User, Favorite} = sequelize.models;
+
+User.belongsToMany( Favorite, {through: "user_favorite" })
+Favorite.belongsToMany( User, {through: "user_favorite" })
+
+
+
+module.exports = {
+   ...sequelize.models,
+   conn: sequelize
+};
